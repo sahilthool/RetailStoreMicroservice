72b888bbba0940db4283ce93b2ae8f38c53f1525
Tejaswini <guttulatejaswini@gmail.com>
2022-09-08 16:26:09 +0530

    TransactionDetailService

--- TransactionDetailService/src/main/java/com/transactiondetail/demo/TransactionDetailServiceApplication.java
+++ TransactionDetailService/src/main/java/com/transactiondetail/demo/TransactionDetailServiceApplication.java
@@ -2,8 +2,12 @@ package com.transactiondetail.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.transactiondetail")
+@EntityScan(basePackages = "com.transactiondetail.bean")
+@EnableJpaRepositories(basePackages = "com.transactiondetail.persistence")
 public class TransactionDetailServiceApplication {
 
 	public static void main(String[] args) {
