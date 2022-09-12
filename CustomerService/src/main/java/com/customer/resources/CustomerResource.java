1975a5b76067f059c4197aa35ec6bde09f353c31
ashika <ashik@DESKTOP-K7PHPH2>
2022-09-12 09:24:27 +0530

    Item service updated

--- CustomerService/src/main/java/com/customer/resources/CustomerResource.java
+++ CustomerService/src/main/java/com/customer/resources/CustomerResource.java
@@ -40,5 +40,4 @@ public class CustomerResource {
 		Customer customer = customerService.getCustomerId(userName);		
 		return customer;
 	}
-
 }
--- ItemService/src/test/java/com/item/demo/ItemServiceApplicationTests.java
+++ ItemService/src/test/java/com/item/demo/ItemServiceApplicationTests.java
@@ -1,13 +1,40 @@
 package com.item.demo;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.item.bean.Item;
+import com.item.service.ItemService;
+
+
 @SpringBootTest
 class ItemServiceApplicationTests {
+	
+	private List<Item> item=new ArrayList<Item>();
+	
+	private ItemService itemService;
 
-	@Test
-	void contextLoads() {
+	@BeforeEach
+	void setUp() throws Exception {
+		item.add(new Item(1,"Book A","Book",20,350));
+		item.add(new Item(2,"Book B","Book",30,500));
+		item.add(new Item(3,"Book C","Book",25,550));
 	}
 
+	@AfterEach
+	void tearDown() throws Exception {
+		item = null;
+	}
+	
+	
+
 }
+
+
